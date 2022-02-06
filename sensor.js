e2a4ca3bf0f2095e84fc37425bec4a923620babd
Nick Townsend <nick.townsend@mac.com>
2022-02-06 22:11:24 +0000

    Use "hostname-sensorname" as serial number
    * To prevent merging of history of multiple devices on same instance
    * Note that this is similar to the naming of the persistent history file
    * which is "hostname_name_persist.json"

--- sensor.js
+++ sensor.js
@@ -66,7 +66,7 @@ class BME280Plugin {
     this.informationService
       .setCharacteristic(Characteristic.Manufacturer, "Bosch")
       .setCharacteristic(Characteristic.Model, "RPI-BME280")
-      .setCharacteristic(Characteristic.SerialNumber, hostname + "-" + hostname)
+      .setCharacteristic(Characteristic.SerialNumber, hostname + "-" + this.name)
       .setCharacteristic(Characteristic.FirmwareRevision, require('./package.json').version);
 
     this.temperatureService = new Service.TemperatureSensor(this.name_temperature);
